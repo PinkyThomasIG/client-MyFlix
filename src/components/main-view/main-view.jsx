3396c745b100b3d45e665a3262e873f750d429ea
PinkyThomasIG <pinkymukalal@gmail.com>
2025-02-21 17:14:08 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,61 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [isSigningUp, setIsSigningUp] = useState(false);
 
   useEffect(() => {
-    fetch("https://movieflix-application-717006838e7d.herokuapp.com/movies")
+    // Check if there's an authToken in localStorage
+    const storedToken = localStorage.getItem("authToken");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch("https://movieflix-application-717006838e7d.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((data) => {
-        console.log("API Response:", data); // Log the entire response here
-        setMovies(data); // Directly set the array of movies
+        setMovies(data); // Set movies only when we successfully get the data
       })
-      .catch((error) => console.error("Error fetching movies:", error));
-  }, []);
+      .catch((error) => console.error("Error fetching movies: ", error));
+  }, [token]);
+
+  if (!user) {
+    return isSigningUp ? (
+      <SignupView
+        onSignedUp={(user) => {
+          setUser(user);
+          setIsSigningUp(false); // Switch to login view after signup
+        }}
+      />
+    ) : (
+      <LoginView
+        onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }}
+      />
+    );
+  }
+
+  // logout functionality
+  const handleLogout = () => {
+    setUser(null); // set user to null to logout
+    setToken(null);
+    localStorage.removeItem("authToken"); // Remove token from localStorage
+  };
 
   // If a movie is selected, render the MovieView component
   if (selectedMovie) {
@@ -34,6 +75,7 @@ export const MainView = () => {
   // Otherwise, map over the movies and render a MovieCard for each
   return (
     <div>
+      <button onClick={handleLogout}>Logout</button>
       {movies.map((movie) => (
         <MovieCard
           key={movie._id} // Use _id as the unique key for each movie
